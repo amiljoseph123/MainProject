53d7528bbdb3e357b6956dbbd61d3d36eed25ea4
amiljoseph123 <amiljoseph2024a@mca.ajce.in>
2024-04-13 15:45:23 +0530

    qr

---
+++ check_gd.php
@@ -0,0 +1,13 @@
+<?php
+// Check if GD extension is loaded
+if (extension_loaded('gd')) {
+    echo 'GD is installed.';
+} else {
+    echo 'GD is not installed.';
+}
+?>
+<?php
+// phpinfo();
+?>
+
+
